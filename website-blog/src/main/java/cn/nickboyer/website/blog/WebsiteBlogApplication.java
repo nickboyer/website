bb6048480ac048a336154b47caa864b96106e48c
nickboyer <nickboyer@foxmail.com>
2017-12-06 17:30:34 +0800

    init blog

---
+++ website-blog/src/main/java/cn/nickboyer/website/blog/WebsiteBlogApplication.java
@@ -0,0 +1,12 @@
+package cn.nickboyer.website.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteBlogApplication.class, args);
+	}
+}
