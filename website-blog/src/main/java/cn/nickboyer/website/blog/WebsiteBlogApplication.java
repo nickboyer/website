e46ab622089a5e3c9155c4eeff74d2abeeb3edb2
nickboyer <nickboyer@foxmail.com>
2017-12-06 18:33:49 +0800

    dubbo

--- website-blog/src/main/java/cn/nickboyer/website/blog/WebsiteBlogApplication.java
+++ website-blog/src/main/java/cn/nickboyer/website/blog/WebsiteBlogApplication.java
@@ -2,8 +2,16 @@ package cn.nickboyer.website.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
+/**
+ * @title
+ * @description
+ * @author Kang.Y
+ * @since JDK1.8
+ */
 @SpringBootApplication
+@ImportResource("classpath:dubbo.xml")
 public class WebsiteBlogApplication {
 
 	public static void main(String[] args) {
